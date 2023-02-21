6f4f9b801b2a4d39040874b7016b2465668ff329
Matheus Mendonça <109557325+MatheuszPorto@users.noreply.github.com>
2023-02-21 07:53:47 -0300

    feat: modules (#4)
    * fix: content align
    
    * feat: modularizing
    
    ---------
    
    Co-authored-by: Matheus Mendonça <mattmen16@gmail.com>

---
+++ js/controls.js
@@ -0,0 +1,8 @@
+function resetControls () {
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+  }
+
+  export default resetControls
